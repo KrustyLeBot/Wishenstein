9a60b2e9d9ce12c36d46caf71982c30f53a31fdf
Jerome <je.bravais@gmail.com>
2024-03-17 23:36:25 +0100

    finish solo game base

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from player import *
 from raycasting import *
 from object_renderer import *
 from object_handler import *
+from weapon import *
+from sound import *
+from pathfinding import *
 
 
 class Game:
@@ -15,36 +18,57 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         pg.event.set_grab(True)  # force mouse to stay focus in game windows
         self.new_game()
+        self.is_over = False
 
     def new_game(self):
+        pg.event.clear()
         self.map = Map(self)
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
+        
 
     def update(self):
         self.player.update()
         self.raycasting.update()
         self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
-        pg.display.set_caption(f"fps: {self.clock.get_fps() :.1f}, pos: {self.player.pos[0] :.1f}, {self.player.pos[1] :.1f}")
+        pg.display.set_caption(
+            f"fps: {self.clock.get_fps() :.1f}, pos: {self.player.pos[0] :.1f}, {self.player.pos[1] :.1f}"
+        )
 
     def draw(self):
         self.object_renderer.draw()
+        self.weapon.draw()
+        # self.screen.fill('black')
         # self.map.draw()
         # self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (
                 event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE
             ):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
+            elif event.type == pg.KEYDOWN and event.key == pg.K_F1 and self.is_over:
+                pg.display.flip()
+                self.new_game()
+            self.player.single_fire_event(event)
 
     def run(self):
         while True:
--- object_handler.py
+++ object_handler.py
@@ -1,12 +1,17 @@
+from random import choices, randrange
 from sprite_object import *
+from npc import *
 
 
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
-        self.static_sprite_path = 'resources/sprites/static_sprites/'
-        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
+        self.npc_list = []
+        self.npc_sprite_path = "resources/sprites/npc/"
+        self.static_sprite_path = "resources/sprites/static_sprites/"
+        self.anim_sprite_path = "resources/sprites/animated_sprites/"
+        self.npc_positions = {}
 
         # sprite map
         add_sprite = self.add_sprite
@@ -19,22 +24,50 @@ class ObjectHandler:
         add_sprite(AnimatedSprite(game, pos=(7.5, 5.5)))
         add_sprite(AnimatedSprite(game, pos=(14.5, 1.5)))
         add_sprite(AnimatedSprite(game, pos=(14.5, 4.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 5.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 7.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(12.5, 7.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(9.5, 7.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 12.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(9.5, 20.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(10.5, 20.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(3.5, 14.5)))
-        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(3.5, 18.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(14.5, 5.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(14.5, 7.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(12.5, 7.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(9.5, 7.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(14.5, 12.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(9.5, 20.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(10.5, 20.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(3.5, 14.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + "red_light/0.png", pos=(3.5, 18.5)))
         add_sprite(AnimatedSprite(game, pos=(14.5, 24.5)))
         add_sprite(AnimatedSprite(game, pos=(14.5, 30.5)))
         add_sprite(AnimatedSprite(game, pos=(1.5, 30.5)))
         add_sprite(AnimatedSprite(game, pos=(1.5, 24.5)))
 
+        # spawn npc
+        self.enemies = 20 
+        self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC]
+        self.weights = [70, 20, 10]
+        self.restricted_area = {(i, j) for i in range(10) for j in range(10)} #no npc in the first 10x10 blocks
+        self.spawn_npc()
+
+    def spawn_npc(self):
+        for i in range(self.enemies):
+                npc = choices(self.npc_types, self.weights)[0]
+                pos = x, y = randrange(self.game.map.cols), randrange(self.game.map.rows)
+                #while npc pos is colliding with walls or in the restricted area, generate new pos
+                while (pos in self.game.map.world_map) or (pos in self.restricted_area):
+                    pos = x, y = randrange(self.game.map.cols), randrange(self.game.map.rows)
+                self.add_npc(npc(self.game, pos=(x + 0.5, y + 0.5)))
+
     def update(self):
+        self.npc_positions = { npc.map_pos for npc in self.npc_list if npc.alive }
         [sprite.update() for sprite in self.sprite_list]
-    
+        [npc.update() for npc in self.npc_list]
+        self.check_win()
+
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
+    
+    def check_win(self):
+        if not len(self.npc_positions):
+            self.game.object_renderer.win()
+            self.game.is_over = True
+            pg.display.flip()
