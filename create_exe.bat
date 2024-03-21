fddeed825bbb6edd2d846c53c638c993ee7e297c
Jerome <je.bravais@gmail.com>
2024-03-21 19:22:39 +0100

    improve exe gen and icon

--- create_exe.bat
+++ create_exe.bat
@@ -1,1 +1,12 @@
-.\pyinstaller --noconsole --onefile main.py
+@RD /S /Q ".\dist"
+python -m PyInstaller --noconsole --icon icon.ico --onefile main.py
+
+@RD /S /Q ".\dist\resources"
+echo d | xcopy /s resources dist\resources
+
+cd dist
+tar -a -cf game.zip main.exe resources
+echo f | xcopy game.zip ..\game.zip
+
+cd..
+@RD /S /Q ".\dist"
